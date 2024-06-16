33080fced066e3b63cb6619986f7a5df4939987a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-16 10:54:26 +0530

    initial commit

---
+++ src/main/java/com/junit/CasDeTestApplication.java
@@ -0,0 +1,13 @@
+package com.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasDeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasDeTestApplication.class, args);
+	}
+
+}
